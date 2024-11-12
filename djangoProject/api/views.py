194ee3ac06bd7042c84cfb41d9b7847fa2df6cce
danielrsh <danielrsh020815@gmail.com>
2024-11-13 03:17:39 +0900

    implemented korean sd service

---
+++ djangoProject/api/views.py
@@ -0,0 +1,122 @@
+import os
+
+from django.shortcuts import render
+from dotenv import load_dotenv
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+
+
+class Login(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        print(_ConfigManager.gpu_machine_url())
+
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        response = requests.post(
+            f'{_ConfigManager.gpu_machine_url()}/users/login/',
+            json={'username': username, 'password': password},
+        )
+
+        if response.status_code == 200:
+            response = response.json()
+            return Response({'success': True, 'refresh': response.get('refresh'), 'access': response.get('access')}, status=status.HTTP_200_OK)
+        else:
+            return Response({'success': False}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PromptRequest(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        access = request.data.get('access')
+        prompt = request.data.get('prompt')
+        negative_prompt = 'text, watermark, nude'
+        workflow = 'kimhongdo'
+        image = None
+
+        # GPU 머신으로 요청 전송
+        response = requests.post(
+            f'{_ConfigManager.gpu_machine_url()}/generation/prompt/',
+            headers={'Authorization': f'{access}', 'Content-Type': 'application/json'},
+            json={'prompt': prompt, 'negative_prompt': negative_prompt, 'workflow': workflow, 'image': image},
+        )
+
+        if response.status_code == 201:
+            return Response({'success': True, 'prompt_id': response.json().get('data').get('prompt_id')}, status=status.HTTP_200_OK)
+        else:
+            return Response({'success': False}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProgressRequest(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        access = request.data.get('access')
+        prompt_id = request.data.get('prompt_id')
+        response = requests.post(
+            f'{_ConfigManager.gpu_machine_url()}/generation/progress/',
+            headers={'Authorization': f'{access}', 'Content-Type': 'application/json'},
+            json={'prompt_id': prompt_id},
+        )
+
+        if response.status_code == 200:
+            return Response({'progress': response.json().get('data').get('progress')}, status=status.HTTP_200_OK)
+        else:
+            return Response({'success': False}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ImageRequest(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        access = request.data.get('access')
+        prompt_id = request.data.get('prompt_id')
+        response = requests.post(
+            f'{_ConfigManager.gpu_machine_url()}/generation/fetch/',
+            headers={'Authorization': f'{access}', 'Content-Type': 'application/json'},
+            json={'prompt_id': prompt_id},
+        )
+
+        if response.status_code == 201:
+            image_url = response.json().get('data').get('image_url')
+            return Response({'image_url': image_url}, status=status.HTTP_200_OK)
+        else:
+            return Response({'success': False}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class _ConfigManager:
+    _loaded = False
+    _gpu_machine_url = None
+    _gpu_server_id = None
+    _gpu_server_password = None
+
+    @classmethod
+    def load(cls):
+        if not cls._loaded:
+            load_dotenv()
+            cls._gpu_machine_url = os.getenv('GPU_MACHINE_URL')
+            cls._gpu_server_id = os.getenv('GPU_SERVER_ID')
+            cls._gpu_server_password = os.getenv('GPU_SERVER_PASSWORD')
+
+    @classmethod
+    def gpu_machine_url(cls):
+        cls.load()
+        return cls._gpu_machine_url
+
+    @classmethod
+    def gpu_server_id(cls):
+        cls.load()
+        return cls._gpu_server_id
+
+    @classmethod
+    def gpu_server_password(cls):
+        cls.load()
+        return cls._gpu_server_password
---
+++ djangoProject/frontend/src/App.js
@@ -0,0 +1,155 @@
+import React, {useEffect, useState} from 'react';
+
+function App() {
+    const [username, setUsername] = useState('testuser');
+    const [access, setAccess] = useState('');
+    const [refresh, setRefresh] = useState('');
+    const [password, setPassword] = useState('test123');
+    const [prompt, setPrompt] = useState('man');
+    const [promptId, setPromptId] = useState('');
+    const [progress, setProgress] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+    const [loginCompleted, setLoginCompleted] = useState(false);
+    const [isInferencing, setIsInferencing] = useState(false);
+
+    const SERVER_URL = 'http://localhost:8001';
+    let progressInterval = null;  // 진행 상태 확인을 위한 interval 변수
+
+    const handleLogin = async () => {
+        try {
+            const response = await fetch(`${SERVER_URL}/api/login/`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ 'username': username, 'password': password })
+            });
+            if (response.ok) {
+                response.json().then(data => {
+                    setAccess(data.access);
+                    setRefresh(data.refresh);
+                })
+                setLoginCompleted(true);
+            }
+        } catch (error) {
+            console.error('Login error:', error);
+        }
+    };
+
+    const handlePromptRequest = async () => {
+        try {
+            const response = await fetch(`${SERVER_URL}/api/prompt/`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ 'access': access, 'prompt': prompt })
+            });
+            if (response.ok) {
+                response.json().then(data => {
+                    setPromptId(data.prompt_id);
+                })
+            }
+        } catch (error) {
+            console.error('Prompt error:', error);
+        }
+    };
+
+    useEffect(() => {
+        // promptId가 업데이트된 이후에 polling 시작
+        if (promptId) {
+            startProgressPolling();
+        }
+    }, [promptId]);  // promptId가 변경될 때만 실행
+
+    const startProgressPolling = () => {
+        if (progressInterval) clearInterval(progressInterval);  // 이전 interval이 있다면 정리
+        setIsInferencing(true);
+        setProgress('0%');
+        progressInterval = setInterval(async () => {
+            await handleProgressRequest();
+        }, 1000);  // 1초 간격으로 진행 상태 요청
+    };
+
+    const stopProgressPolling = () => {
+        if (progressInterval) {
+            clearInterval(progressInterval);  // interval 정리
+            progressInterval = null;
+            setIsInferencing(false);
+        }
+    };
+
+    const handleProgressRequest = async () => {
+        try {
+            const response = await fetch(`${SERVER_URL}/api/progress/`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ 'prompt_id': promptId })
+            });
+            const data = await response.json();
+            if (data.progress === 100) {
+                setProgress(`${data.progress}%`);
+                await handleImageRequest();
+                stopProgressPolling();
+            } else {
+                setProgress(`${data.progress}%`);
+            }
+        } catch (error) {
+            console.error('Progress error:', error);
+        }
+    };
+
+    const handleImageRequest = async () => {
+        try {
+            const response = await fetch(`${SERVER_URL}/api/image/`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ 'prompt_id': promptId })
+            });
+            const data = await response.json();
+            setImageUrl(data.image_url);
+        } catch (error) {
+            console.error('Image request error:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Korean Painter Style Diffusion</h1>
+
+            <div>
+                <input
+                    type="text"
+                    placeholder="ID"
+                    value={username}
+                    disabled={loginCompleted}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    disabled={loginCompleted}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button onClick={handleLogin} disabled={loginCompleted}>
+                    {loginCompleted ? 'Login Completed' : 'Login'}
+                </button>
+            </div>
+
+            <div>
+                <input
+                    type="textarea"
+                    placeholder="Prompt"
+                    value={prompt}
+                    onChange={(e) => setPrompt(e.target.value)}
+                />
+                <button onClick={handlePromptRequest} disabled={isInferencing}>Submit Prompt</button>
+            </div>
+
+            <div>
+                <p>Prompt ID: {promptId}</p>
+                <p>Progress: {progress}</p>
+                {imageUrl && <img src={imageUrl} alt="Generated" />}
+            </div>
+        </div>
+    );
+}
+
+export default App;
